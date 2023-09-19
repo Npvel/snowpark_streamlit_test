64314ed787bf91820f863edc791b3242c7205803
Npvel <32815106+Npvel@users.noreply.github.com>
2023-09-19 15:54:06 +0200

    Update streamlit_app.py
    labs exercises

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title("TEST SNOWPARK DEPLOY")
 
 st.header("I am playing around")
+
+st.title("MENU FROM LABS")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
