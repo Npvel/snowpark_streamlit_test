edafa72d05fd3aecb8d82aeb759d789312cb8319
Npvel <32815106+Npvel@users.noreply.github.com>
2023-09-19 16:00:38 +0200

    Update streamlit_app.py
    add dataframe from S3 labs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.title("TEST SNOWPARK DEPLOY")
 
@@ -12,3 +13,7 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text("🥑🍞 Avacado toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+st.dataframe(my_fruit_list)
