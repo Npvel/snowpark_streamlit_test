399e6209ea173d984ccf17f4fe2e703ee129662b
Npvel <32815106+Npvel@users.noreply.github.com>
2023-09-19 15:57:57 +0200

    Update streamlit_app.py
    Add emodji and add the new headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,10 @@ st.title("TEST SNOWPARK DEPLOY")
 st.header("I am playing around")
 
 st.title("MENU FROM LABS")
-
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text("🥑🍞 Avacado toast")
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
