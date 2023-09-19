0c5ffd27622df7edf5c0300956deafd51257d5e9
Npvel <32815106+Npvel@users.noreply.github.com>
2023-09-19 15:54:53 +0200

    Update streamlit_app.py
    fix streamlit into st.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.header("I am playing around")
 
 st.title("MENU FROM LABS")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
